2ec1530a15662fcb613647d7e9dcd4e03aba79fa
Marco Siebecke <marco@siebecke.se>
2022-11-20 23:03:16 +0100

    test4.jpg got correctly recognised

--- scripts/ocr.py
+++ scripts/ocr.py
@@ -15,6 +15,7 @@ import cv2
 log = logging.getLogger(__file__)
 
 DEBUG = True
+NON_ZERO_PIXEL_THRESHOLD = 0.4
 
 
 def _configure_logging(verbosity):
@@ -26,16 +27,9 @@ def _configure_logging(verbosity):
             logging.getLogger(loggername).setLevel(logging.CRITICAL)
 
 
-def test_image(img):
-    cv2.imshow('', img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-
 # define the dictionary of digit segments so we can identify
 # each digit on the thermostat
 DIGITS_LOOKUP = {
-    (1, 1, 0, 0, 0, 0, 0): 100,
     (1, 1, 1, 0, 1, 1, 1): 0,
     (0, 0, 1, 0, 0, 1, 0): 1,
     (1, 0, 1, 1, 1, 1, 0): 2,
@@ -49,22 +43,10 @@ DIGITS_LOOKUP = {
 }
 
 
-def grabcut_algorithm(original_image, bounding_box):
-
-    image = original_image.copy()
-    segment = np.zeros(image.shape[:2], np.uint8)
-
-    x, y, width, height = bounding_box
-    segment[y:y + height, x:x + width] = 1
-
-    background_mdl = np.zeros((1, 65), np.float64)
-    foreground_mdl = np.zeros((1, 65), np.float64)
-
-    cv2.grabCut(image, segment, bounding_box, background_mdl, foreground_mdl, 5, cv2.GC_INIT_WITH_RECT)
-
-    new_mask = np.where((segment == 2) | (segment == 0), 0, 1).astype('uint8')
-
-    return image * new_mask[:, :, np.newaxis]
+def test_image(img):
+    cv2.imshow('', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
 def get_edges(imagepath: str):
@@ -77,7 +59,8 @@ def get_edges(imagepath: str):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     blurred = cv2.GaussianBlur(gray, (5, 5), 0)
     edged = cv2.Canny(blurred, 50, 200, 255)
-    test_image(edged)
+    if DEBUG:
+        test_image(edged)
 
     # find contours in the edge map, then sort them by their
     # size in descending order
@@ -95,19 +78,25 @@ def get_edges(imagepath: str):
         if len(approx) == 4:
             displayCnt = approx
             break
+
+    if not displayCnt:
+        log.fatal("Did not find display in image")
+        sys.exit(1)
+
     # extract the thermostat display, apply a perspective transform
     # to it
     warped = four_point_transform(gray, displayCnt.reshape(4, 2))
     output = four_point_transform(image, displayCnt.reshape(4, 2))
-
-    test_image(output)
+    if DEBUG:
+        test_image(output)
 
     # threshold the warped image, then apply a series of morphological
     # operations to cleanup the thresholded image
     thresh = cv2.threshold(warped, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 5))
     thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
-    test_image(thresh)
+    if DEBUG:
+        test_image(thresh)
 
     # find contours in the thresholded image, then initialize the
     # digit contours lists
@@ -125,10 +114,9 @@ def get_edges(imagepath: str):
         # if the contour is sufficiently large, it must be a digit
         if w >= 60 and (h >= 100 and h <= 200):
             digitCnts.append(c)
-            # cv2.putText(output, "({w},{h})".format(w=w, h=h), (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.65,
-            #             (0, 255, 0), 2)
 
-    test_image(output)
+    if DEBUG:
+        test_image(output)
 
     # sort the contours from left-to-right, then initialize the
     # actual digits themselves
@@ -175,17 +163,16 @@ def get_edges(imagepath: str):
             # extract the segment ROI, count the total number of
             # thresholded pixels in the segment, and then compute
             # the area of the segment
-            segROI = roi[yA:yB, xA:xB]
+            segROI = roi[yA:yA + yB, xA:xA + xB]
             total = cv2.countNonZero(segROI)
-            area = (xB - xA) * (yB - yA)
-            print(area)
+            area = xB * yB
             # if the total number of non-zero pixels is greater than
             # 50% of the area, mark the segment as "on"
             if area:
-                if total / float(area) > 0.1:
+                if total / float(area) > NON_ZERO_PIXEL_THRESHOLD:
                     on[i] = 1
         # lookup the digit and draw it on the image
-        digit = DIGITS_LOOKUP[tuple(on)]
+        digit = DIGITS_LOOKUP.get(tuple(on))
         digits.append(digit)
         cv2.rectangle(output, (x, y), (x + w, y + h), (0, 255, 0), 1)
         cv2.putText(output, str(digit), (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 255, 0), 2)
@@ -196,12 +183,18 @@ def get_edges(imagepath: str):
 
 @click.command()
 @click.option('-v', '--verbosity', help='Verbosity', default=0, count=True)
+@click.option('-d', '--debug', help='Show images etc for debugging purposes', default=False)
+@click.option('-t',
+              '--nonzero-pixel-threshold',
+              help='percentage of non zero picels in a cell to read as lit',
+              default=0.4)
 @click.argument('imagepath')
-def cli(verbosity: int, imagepath: str):
+def cli(verbosity: int, debug: bool, nonzero_pixel_threshold: float, imagepath: str):
     """ main program
     """
     _configure_logging(verbosity)
 
+    DEBUG = debug
     get_edges(imagepath)
 
     return 0
